13b75e7ec2597b8d3f5571ce729e578eec12bbbe
Valekh Bayramov <bayvd02@gmail.com>
2021-03-12 11:14:27 +0300

    last sections

--- js/application.js
+++ js/application.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
+    $(".slider").css("width", `${($('.reviews__single').length) * 160}px`)
 
+    var offset = setupSlides();
     $(".btn-search").hover(
         function () {
             $(".search-field").addClass('hover')
@@ -16,17 +18,32 @@ $(document).ready(function () {
         $(".search-field").removeClass('hover')
     })
     $("#slider").slider({
-        range: 'min',
+        range: 'false',
+        animate: "fast",
         min: 0,
         max: ($('.reviews__single').length-1),
-        start: function(){
-            $(".ui-slider-horizontal").css("width", $('.reviews__single').length*160+"px")
+        start: function () {
+            
         },
         slide: function (event, ui) {
             console.log("previous value:" + $(this).slider('option', 'value'));
         },
         stop: function (event, ui) {
-            console.log("Current value:" + $(this).slider('option', 'value'));
+            var current = $(this).slider('option', 'value');
+
+            $(".reviews .row").css("transform", `translateX(${offset*(1-current)}%)`);
+            console.log("Current value:" + current);
         }
     });
 });
+
+function setupSlides() {
+    var slideWidth = parseInt($(".reviews .col-lg-4").css("width"), 10);
+    var rowWidth = parseInt($(".reviews .row").css("width"), 10);
+    var offset = (slideWidth * 100 / rowWidth);
+    $(".reviews .row").css("transform", `translateX(${offset}%)`);
+    $(".reviews .col-lg-4").each(function (i) {
+        $(this).css("left", `${offset*i}%`)
+    })
+    return offset;
+}
