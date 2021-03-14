b152f279b2144fe32271c500c88fac89f1352281
Valekh Bayramov <bayvd02@gmail.com>
2021-03-14 18:20:15 +0300

    done with html

--- js/application.js
+++ js/application.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
-    $(".slider").css("width", `${($('.reviews__single').length) * 160}px`)
-
+    
     var offset = setupSlides();
     $(".btn-search").hover(
         function () {
@@ -21,16 +20,17 @@ $(document).ready(function () {
         range: 'false',
         animate: "fast",
         min: 0,
-        max: ($('.reviews__single').length-1),
-        start: function () {
-            
+        max: ($('.reviews__single').length)-1,
+        value: Math.floor($('.reviews__single').length / 2),
+        create: function () {
+            var current = Math.floor($('.reviews__single').length / 2);
+            $(".reviews .row").css("transform", `translateX(${offset*(1-current)}%)`);
         },
         slide: function (event, ui) {
             console.log("previous value:" + $(this).slider('option', 'value'));
         },
         stop: function (event, ui) {
             var current = $(this).slider('option', 'value');
-
             $(".reviews .row").css("transform", `translateX(${offset*(1-current)}%)`);
             console.log("Current value:" + current);
         }
